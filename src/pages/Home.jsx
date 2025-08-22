6e09c4ef3845b265f36b3a1784aec7e5a3bf25f0
Fathur <114320333+fatkhurrhn@users.noreply.github.com>
2025-08-22 18:03:17 +0700

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import BottomNav from '../components/BottomNav'
 
 export default function Home() {
   return (
-    <div className='text-red-900 font-bold text-center pt-20'>Home</div>
+    <div className='text-red-900 font-bold text-center pt-20'>
+      <BottomNav/>
+      <div><h1>helo</h1></div>
+    </div>
   )
 }
