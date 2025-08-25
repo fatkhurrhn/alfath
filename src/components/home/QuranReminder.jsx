7e68ee41f2d1fcfe026788124370094f0a5e5126
Fathur <114320333+fatkhurrhn@users.noreply.github.com>
2025-08-26 00:43:23 +0700

    updated

--- src/components/home/QuranReminder.jsx
+++ src/components/home/QuranReminder.jsx
@@ -10,7 +10,7 @@ export default function QuranReminder() {
       </p>
       <div className="flex items-center space-x-3">
         <Link
-          to="/alquran"
+          to="/quran"
           className="px-4 py-2 bg-white text-green-700 font-medium text-sm rounded-lg shadow hover:bg-gray-100 transition"
         >
           Baca Sekarang
--- src/pages/alquran/Games.jsx
+++ src/pages/alquran/Games.jsx
@@ -146,7 +146,7 @@ export default function Game() {
             <div className="max-w-xl mx-auto px-3 container border-x border-gray-200">
                 <div className="fixed max-w-xl border border-gray-200 mx-auto top-0 left-1/2 -translate-x-1/2 w-full z-50 bg-white px-3 py-4">
                     <div className="flex items-center justify-between">
-                        <Link to="/game" className="flex items-center font-semibold gap-2 text-gray-800 text-[15px]">
+                        <Link to="/" className="flex items-center font-semibold gap-2 text-gray-800 text-[15px]">
                             <i className="ri-arrow-left-line"></i> Games Have Fun
                         </Link>
                         <button className="text-gray-600 hover:text-gray-600">
